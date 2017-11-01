966e889bf6880f321e5e57a04fdd290bd43994a8
guillaume <guillaume.torres91@gmail.com>
2017-11-01 22:19:41 +0100

    Fix path error

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ let bodyParser = require('body-parser')
 let env = require('node-env-file')
 let routes = require('./routes/index')
 
-env(__dirname + '/src/.env');
+env(__dirname + '/.env');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
